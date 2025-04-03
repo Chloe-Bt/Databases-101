da4b81fefc2d4aa5dbd31f56d02cb31834e79f35
Chloe-Boot <50842809+Chloe-Boot@users.noreply.github.com>
2025-04-03 10:48:37 +0200

    "Make data manipulations.sql" ~Chloe

--- data manipulation.sql
+++ data manipulation.sql
@@ -1,8 +1,31 @@
+-- GROUP BY, HAVING: Which shops have more than 5 of the book 'The Hobbit' in stock?
 SELECT '<query 1>';
-SELECT '<query 2>';
-SELECT '<query 3>';
+Select bs.name, bs.address, b.title, s.stock from Bookshop bs 
+	join Stocks s on bs.address = s.address
+    Join Book b on  s.isbn = b.isbn
+    where b.title = 'The Hobbit'
+    group by bs.address having stock > 5;
 
--- 
+
+-- Set operation (UNION, INTERSECT, EXCEPT)
+-- Compute the names of the employees who have a manager function.
+SELECT '<query 2>';
+SELECT name FROM Employee
+INTERSECT
+SELECT employeeID FROM Contract
+    Where function = 'manager';
+
+-- Nested query: books with the same genre as 'Good Omens'?
+SELECT '<query 3>';
+SELECT b.title from Book b 
+	join BookGenre bg on b.isbn = bg.isbn
+	where bg.genreID in 
+    	(SELECT bg.genreID from BookGenre bg 
+     		where bg.isbn in 
+     			(SELECT b.isbn from Book b 
+                 	where b.title = 'Good Omens')
+    );
+
 SELECT '<query 4>';
 SELECT B.title, A.name FROM Author A, Book B, BookAuthor BA
     WHERE B.isbn = BA.isbn
@@ -14,5 +37,16 @@ SELECT B.title, A.name FROM Author A, Book B, BookAuthor BA
         HAVING COUNT(authorID) >= 2
     );
 
+-- Query over multiple tables (JOIN): Which employees have a contract at the shop in Booktown?
 SELECT '<query 5>';
+select e.employeeID, e.name from Employee e
+join Contract c on c.employeeID = e.employeeID
+where c.address like '%Booktown';
+
+
+-- LIKE (string matching)
+-- Compute the title of every book beginning with "The".
 SELECT '<query 6>';
+SELECT * 
+    FROM Book
+    WHERE title LIKE 'The%';
